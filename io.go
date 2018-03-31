0d77fd38d94bbc0382d44a49ecf1f124677605d5
Andreas <mua@daneb.de>
2018-04-01 01:16:47 +0200

    Initial commit.

---
+++ io.go
@@ -0,0 +1,362 @@
+package stellarwallet
+
+import(
+	"errors"
+	"fmt"
+	"io"
+	"bytes"
+	"encoding/binary"
+	"crypto/sha1"	
+	"crypto/hmac"
+	"compress/gzip"
+
+	//"encoding/hex"
+
+)
+
+const maxLen = 65535
+const tagWalletDesc = 1
+const tagWalletMasterSeed = 2
+const tagWalletBip39Seed = 3
+const tagSep0005AccountCount = 4
+const tagAccount = 5
+const tagAccountDesc = 6
+const tagAccountType = 7
+const tagAccountPublicKey = 8
+const tagAccountPrivateKey = 9
+const tagAccountSep005DerivationPath = 10
+const tagWalletStart = 100
+const tagWalletEnd = 101
+
+
+func writeUint16(w io.Writer, data uint16) {
+	err := binary.Write(w, binary.BigEndian, data)
+	if err != nil {
+		panic("failed writing integer to buffer: " + err.Error())
+	}
+}
+
+func readUint16(r io.Reader) (data uint16, err error) {
+	var d uint16
+	
+	err = binary.Read(r, binary.BigEndian, &d)
+
+	if err != nil {
+		err = errors.New("failed to read from buffer: " + err.Error())
+	}
+
+	return d, err
+}
+
+func writeBytes(w io.Writer, buf []byte) {
+	count := len(buf) 
+
+	if count > maxLen {
+		panic("buffer too big")
+	}
+
+	writeUint16(w, uint16(count))
+
+	n, err := w.Write(buf)
+	
+	if n != count {
+		if err != nil {
+			panic("failed writing byte data to buffer: " + err.Error())
+		} else {
+			panic("failed writing byte data to buffer")
+		}
+	}
+}
+
+func readBytes(r io.Reader) (buf []byte, err error) {
+	var count int
+
+	d, err := readUint16(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	count = int(d)
+
+	//fmt.Printf("readBytes: count: %d\n", count)
+
+	buf = make([]byte, count)
+
+	n, err := r.Read(buf)
+
+	if n != count {
+		if err != nil {
+			return nil, errors.New("failed reading byte data from buffer: " + err.Error())
+		} else {
+			return nil, errors.New("failed reading byte data from buffer")
+		}
+	}
+
+	err = nil
+
+	return
+}
+
+func writeString(w io.Writer, s string) {
+	writeBytes(w, []byte(s))
+}
+
+func readString(r io.Reader) (s string, err error) {
+	buf, err := readBytes(r)
+
+	if err != nil {
+		return "", err
+	} else {
+		return string(buf), nil
+	}
+}
+
+func writeTag(w io.Writer, tag byte) {
+	buf := []byte{ tag }
+
+	n, err := w.Write(buf)
+	
+	if n != 1 {
+		if err != nil {
+			panic("failed writing tag to buffer: " + err.Error())
+		} else {
+			panic("failed writing tag to buffer")
+		}
+	}
+}
+
+func readTag(r io.Reader) (tag byte, err error) {
+	buf := make([]byte, 1)
+
+	n, err := r.Read(buf)
+
+	if n != 1 {
+		if err != nil {
+			return 0, errors.New("failed reading tag from buffer: " + err.Error())
+		} else {
+			return 0, errors.New("failed reading tag from buffer")
+		}
+	}
+
+	//fmt.Printf("Tag: %d\n", buf[0])
+
+	return buf[0], nil
+}
+
+func (w *Wallet) writeToBuffer() []byte {
+	buf := new(bytes.Buffer)
+
+	writeTag(buf, tagWalletStart)
+
+	writeTag(buf, tagWalletDesc)
+	writeString(buf, w.desc)
+
+	writeTag(buf, tagWalletMasterSeed)
+	writeBytes(buf, w.masterSeed)
+
+	if w.bip39Seed != nil {
+		writeTag(buf, tagWalletBip39Seed)
+		writeBytes(buf, w.bip39Seed)
+
+		writeTag(buf, tagSep0005AccountCount)
+		writeUint16(buf, w.sep0005AccountCount)
+	}
+
+	for i, _ := range w.accounts {
+		a := &w.accounts[i]
+
+		if a.active {
+			writeTag(buf, tagAccount)
+
+			writeTag(buf, tagAccountDesc)
+			writeString(buf, a.desc)
+			
+			writeTag(buf, tagAccountType)
+			writeUint16(buf, a.accountType)
+			
+			writeTag(buf, tagAccountPublicKey)
+			writeString(buf, a.publicKey)
+			
+			if a.privateKey != nil {
+				writeTag(buf, tagAccountPrivateKey)
+				writeBytes(buf, a.privateKey)
+			}
+
+			if len(a.sep0005DerivationPath) > 0 {
+				writeTag(buf, tagAccountSep005DerivationPath)
+				writeString(buf, a.sep0005DerivationPath)
+			}
+		}
+	}
+
+	writeTag(buf, tagWalletEnd)
+
+	data := make([]byte, buf.Len())
+	copy(data, buf.Bytes())
+
+	mac := hmac.New(sha1.New, []byte(stellarwalletSeed))
+	_, err := mac.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	sum := mac.Sum(nil)
+
+	data = append(data, sum...)
+
+	return data
+}
+
+func (w *Wallet) readFromBuffer(buf []byte) error {
+
+	// verify checksum
+	mac := hmac.New(sha1.New, []byte(stellarwalletSeed))
+
+	if len(buf) < mac.Size() {
+		return errors.New("reader: checksum failed")
+	}
+
+	_, err := mac.Write(buf[:len(buf)-mac.Size()])
+	if err != nil {
+		panic(err)
+	}
+	sum := mac.Sum(nil)
+
+	if !hmac.Equal(sum, buf[len(buf)-mac.Size():]) {
+		return errors.New("reader: checksum failed")
+	}
+
+	// parse buffer
+	var a *Account
+	r := bytes.NewReader(buf)
+
+	tag, err := readTag(r)
+	if err != nil { return err }
+
+	if tag != tagWalletStart {
+		return errors.New("missing start tag")
+	}
+
+	for stop:= false; !stop;  {
+		tag, err := readTag(r)
+		if err != nil { return err }
+
+		switch tag {
+		case tagWalletDesc:
+			s, err := readString(r)
+			if err != nil { return err }
+			w.desc = s
+
+		case tagWalletMasterSeed:
+			buf, err := readBytes(r)
+			if err != nil { return err }
+			w.masterSeed = buf
+
+		case tagWalletBip39Seed:
+			buf, err := readBytes(r)
+			if err != nil { return err }
+			w.bip39Seed = buf
+			
+		case tagSep0005AccountCount:
+			d, err := readUint16(r)
+			if err != nil { return err }
+			w.sep0005AccountCount = d
+
+		case tagAccount:
+			a = w.newAccount()
+			a.active = true
+
+		case tagAccountDesc:
+			if a == nil { return errors.New("unexpected account tag") }
+			s, err := readString(r)
+			if err != nil { return err }
+			a.desc = s
+				
+		case tagAccountType:
+			if a == nil { return errors.New("unexpected account tag") }
+			d, err := readUint16(r)
+			if err != nil { return err }
+			a.accountType = d
+
+		case tagAccountPublicKey:
+			if a == nil { return errors.New("unexpected account tag") }
+			s, err := readString(r)
+			if err != nil { return err }
+			a.publicKey = s
+
+		case tagAccountPrivateKey:
+			if a == nil { return errors.New("unexpected account tag") }
+			buf, err := readBytes(r)
+			if err != nil { return err }
+			a.privateKey = buf
+
+		case tagAccountSep005DerivationPath:
+			if a == nil { return errors.New("unexpected account tag") }
+			s, err := readString(r)
+			if err != nil { return err }
+			a.sep0005DerivationPath = s
+
+		case tagWalletEnd:
+			stop = true
+
+		default:
+			return errors.New(fmt.Sprintf("invalid tag found: %x", tag) )
+		}
+
+	}
+
+
+	return nil
+}
+
+func (w *Wallet) writeToBufferCompressed() []byte {
+	buf := w.writeToBuffer()
+
+	bufComp := new(bytes.Buffer)
+
+	compress, err := gzip.NewWriterLevel(bufComp, gzip.BestCompression)
+	if err != nil { panic("compressing failed: " + err.Error()) }
+
+	n, err := compress.Write(buf)
+	if err != nil { panic("compressing failed: " + err.Error()) }
+	if n != len(buf) { panic("compressing failed: " + err.Error()) }
+
+		
+	err = compress.Close()
+	if err != nil { panic("compressing failed: " + err.Error()) }
+
+	return bufComp.Bytes()
+}
+
+
+func (w *Wallet) readFromBufferCompressed(buf []byte) error {
+	r := bytes.NewReader(buf)
+	
+	decompress, err := gzip.NewReader(r)
+	if err != nil { return errors.New("de-compressing failed: " + err.Error()) }
+	
+	blkLen := 10
+
+	tmp := make([]byte, blkLen)
+	var result []byte
+
+	for cont := true; cont; {
+		n, err := decompress.Read(tmp)
+		if n != 0 {
+			//fmt.Printf("Decompress tmp: %s\n", hex.EncodeToString(tmp[:n]))
+			result = append(result, tmp[:n]...)
+		} else {
+			if err == nil || err == io.EOF {
+				cont = false
+			} else {
+				return errors.New("de-compressing failed: " + err.Error())
+			}
+		}
+	}
+	err = decompress.Close()
+	if err != nil { return errors.New("de-compressing failed: " + err.Error()) }
+
+	//fmt.Printf("Decompress: %s\n", hex.EncodeToString(result))
+	
+	return w.readFromBuffer(result)
+}
---
+++ wallet.go
@@ -0,0 +1,828 @@
+package stellarwallet
+
+import(
+	"fmt"
+	"errors"
+	"strings"
+	"bytes"
+
+	"crypto/sha1"	
+	"crypto/aes"
+	"crypto/hmac"
+	"crypto/rand"
+
+	"encoding/base64"
+
+	"golang.org/x/crypto/pbkdf2"	
+	"golang.org/x/crypto/ed25519"
+
+	"github.com/bartekn/go-bip39"
+	"github.com/stellar/go/strkey"
+	"github.com/stellar/go/exp/crypto/derivation"
+	//"github.com/stellar/go/keypair"
+
+	//"encoding/hex"
+
+)
+
+
+const stellarwalletSeed = "sellarwallet seed"
+
+const masterSeedLen = 32
+const bip39SeedLen = 64
+const accountSeedLen = 32
+
+const AccountTypeUndefined = 0 // Account type undefined
+const AccountTypeSEP0005 = 1 // Account is SEP0005 derived from the BIP39 seed
+const AccountTypeRandom = 2  // Account is based on a randomly generated key
+const AccountTypeWatching = 3 // Account has a public key only 
+const AccountTypeAddressBook = 4 // Account is an address book entry (public key only) 
+
+
+type Account struct {
+	wallet *Wallet
+	active bool
+	desc string
+	accountType uint16
+	publicKey string
+	privateKey []byte
+	sep0005DerivationPath string
+}
+
+type Asset struct {
+	desc string
+	issuer string
+	assetId string
+}	
+
+type Wallet struct {
+	desc string
+
+	masterSeed []byte
+
+	bip39Seed []byte
+
+	sep0005AccountCount uint16
+
+	accounts []Account
+	assets []Asset
+}
+
+func EraseByteBuffer(b []byte) {
+	if b != nil {
+		for i, _ := range b {
+			b[i] = 0
+		}
+	}
+}
+
+/*
+func EraseString(s string) {
+	for i, _ := range s {
+		s[i] = 'x'
+	}
+}
+*/
+
+func NewWallet(password *string) *Wallet {
+	wallet := new(Wallet)
+
+	entropy, err := bip39.NewEntropy(256)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	key := deriveAesKey(password)
+
+	wallet.encryptMasterSeed(entropy, key)
+
+	EraseByteBuffer(entropy)
+	EraseByteBuffer(key)
+	
+	return wallet
+}
+
+func NewWalletFromMnemonic(walletPassword *string, mnemonic []string, mnemonicPassword *string) *Wallet {
+	w := new(Wallet)
+
+	key := deriveAesKey(walletPassword)
+	defer EraseByteBuffer(key)
+
+	seed, err := bip39.MnemonicToByteArray(strings.Join(mnemonic, " "))
+	if err != nil {
+		return nil
+	}
+	defer EraseByteBuffer(seed)
+	
+	seed = seed[:masterSeedLen]
+
+	if len(seed) != masterSeedLen {
+		fmt.Printf("seed len: %d", len(seed))
+		panic("invalid master seed derived " )
+	}
+
+	w.encryptMasterSeed(seed, key)
+
+	if !w.generateBip39Seed(key, mnemonic, mnemonicPassword) {
+		return nil
+	}
+
+	return w
+}
+
+func ImportBinary(buf []byte) (w *Wallet, err error) {
+	w = new(Wallet)
+
+	err = w.readFromBufferCompressed(buf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+func ImportBase64(data string) (w *Wallet, err error) {
+	buf, err := base64.StdEncoding.DecodeString(data)
+
+	if err != nil {
+		return nil, errors.New("base64 decoding failed: " + err.Error())
+	}
+
+	w = new(Wallet)
+
+	err = w.readFromBufferCompressed(buf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+func (w *Wallet)ExportBinary() []byte {
+	return w.writeToBufferCompressed()
+}
+
+func (w *Wallet)ExportBase64() string {
+	buf := w.writeToBufferCompressed()
+
+	if buf == nil {
+		return ""
+	} else {
+		return base64.StdEncoding.EncodeToString(buf)
+	}
+}
+
+func (w *Wallet)clearAccounts() {
+	for _, a := range w.accounts {
+		a.active = false
+		a.privateKey = nil
+		a.publicKey = ""
+		a.sep0005DerivationPath = ""
+	}
+}
+
+
+
+func deriveAesKey(password *string) (key []byte) {
+	return pbkdf2.Key([]byte(*password), []byte("stellarwallet"), 4096, 32, sha1.New)
+}
+
+func aesEncrypt( data, key []byte) []byte {
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		panic(err)
+	}
+
+	l := len(data)
+	blockLen := block.BlockSize()
+
+	blocks := l / blockLen
+
+	padding :=  l % blockLen
+
+	if padding != 0 {
+		blocks += 1
+	}
+
+	buf := make([]byte, blockLen*blocks)
+
+	if padding != 0 {
+		// fill padding bytes with random numbers
+		_, err = rand.Read(buf[blockLen*(blocks-1)+padding:])
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	copy(buf, data)
+
+	itr := buf
+
+	for i := 0; i < blocks; i++ {
+		block.Encrypt(itr, itr)
+		itr = itr[blockLen:]
+	}
+
+	return buf
+} 
+
+func aesDecrypt( data, key []byte) {
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		panic(err)
+	}
+
+
+	l := len(data)	
+	blockLen := block.BlockSize()
+	blocks := l / blockLen
+
+	if l % blockLen != 0 {
+		return
+	}
+
+	itr := data
+
+	for i := 0; i < blocks; i++ {
+		block.Decrypt(itr, itr)
+		itr = itr[blockLen:]
+	}
+} 
+
+func encryptWithCheckSum(data, key []byte) []byte {
+	// build checksum using a hash
+	mac := hmac.New(sha1.New, []byte(stellarwalletSeed))
+	_, err := mac.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	sum := mac.Sum(nil)
+
+	dataChk := make([]byte, len(data))
+	copy(dataChk, data)
+	dataChk = append(dataChk, sum...)
+	defer EraseByteBuffer(dataChk)
+
+	enc := aesEncrypt(dataChk, key)
+
+	return enc
+
+}
+
+func decryptWithCheckSum(encData []byte, resLen int, key []byte) []byte {
+	buf := make([]byte, len(encData))
+	copy(buf, encData)
+	
+	aesDecrypt(buf, key)
+
+	if len(buf) < resLen {
+		return nil
+	}
+
+	// check checksum using a hash
+	mac := hmac.New(sha1.New, []byte(stellarwalletSeed))
+	_, err := mac.Write(buf[:resLen])
+	if err != nil {
+		panic(err)
+	}
+	sum := mac.Sum(nil)
+
+	if !hmac.Equal(sum, buf[resLen:resLen+mac.Size()]) {
+		return nil
+	}
+
+	return buf[:resLen]
+}
+
+func (w *Wallet)getBip39Mnemonic(key []byte) (words []string) {
+
+	seed := w.decryptMasterSeed(key)
+
+	if seed == nil {
+		return
+	}
+
+	defer EraseByteBuffer(seed)
+	
+	wordString, err := bip39.NewMnemonic(seed)
+
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	words = strings.Split(wordString, " ")
+
+	return
+}
+
+func (w *Wallet)GetBip39Mnemonic(walletPassword *string) (words []string) {
+
+	key := deriveAesKey(walletPassword)
+
+	defer EraseByteBuffer(key)
+
+	words = w.getBip39Mnemonic(key)
+
+	return
+}
+
+func (w *Wallet)encryptMasterSeed(seed, key []byte) {
+	w.masterSeed = encryptWithCheckSum(seed, key)
+}	
+
+func (w *Wallet)decryptMasterSeed(key []byte) []byte {
+	return decryptWithCheckSum(w.masterSeed, masterSeedLen, key)
+}	
+
+// Checks if given wallet password is valid.
+// Returns derived AES key on success else nil
+func (w *Wallet)checkPassword(walletPassword *string) []byte {
+	key := deriveAesKey(walletPassword)
+
+	seed := w.decryptMasterSeed(key)
+
+	if seed != nil {
+		EraseByteBuffer(seed)
+		return key
+	}
+
+	return nil
+}
+
+// Checks if given wallet password is valid.
+func (w *Wallet)CheckPassword(walletPassword *string) bool {
+	key := w.checkPassword(walletPassword)
+
+	if key != nil {
+		EraseByteBuffer(key)
+		return true
+	}
+
+	return false
+}
+
+
+
+func (w *Wallet)encryptBip39Seed(seed, key []byte) {
+	w.bip39Seed = encryptWithCheckSum(seed, key)
+}	
+
+func (w *Wallet)decryptBip39Seed(key []byte) []byte {
+	return decryptWithCheckSum(w.bip39Seed, bip39SeedLen, key)
+}	
+
+func (w *Wallet)generateBip39Seed(key []byte, words []string , mnemonicPassword *string) bool {
+	var seed, prevSeed []byte
+	var err error
+
+	mnemonic := strings.Join(words, " ")
+
+	// paranoia mode: calculate seed 5 times and check for identical results
+	// to reduce risk of generating an unreproducable seed caused by faulty hardware
+	for i := 0 ; i < 5; i++ {
+		prevSeed = seed
+		seed, err = bip39.NewSeedWithErrorChecking(mnemonic, *mnemonicPassword)
+		if err != nil {
+			return false
+		}
+
+		if prevSeed != nil && bytes.Compare(prevSeed, seed) != 0 {
+			panic("calculation error")
+		}
+	}	
+
+	if len(seed) != bip39SeedLen {
+		panic("Unexpected length of seed")
+	}
+
+	w.encryptBip39Seed(seed, key)
+
+	return true
+}
+
+func (w *Wallet)GenerateBip39Seed(walletPassword *string, mnemonicPassword *string) bool {
+	if w.bip39Seed != nil {
+		return false
+	}
+
+	key := deriveAesKey(walletPassword)
+
+	words := w.getBip39Mnemonic(key)
+
+	if words == nil {
+		// may happen if wallet password is not correct
+		return false
+	}
+
+
+	return w.generateBip39Seed(key, words, mnemonicPassword)
+}
+
+func (w *Wallet)ChangePassword(password, newPassword *string) bool {
+	key :=  w.checkPassword(password)
+	if key == nil {
+		return false
+	}
+	defer EraseByteBuffer(key)
+
+	newKey := deriveAesKey(newPassword)
+	defer EraseByteBuffer(newKey)
+
+	masterSeed := w.decryptMasterSeed(key)
+	if masterSeed == nil {
+		panic("decrypting master seed failed")
+	}
+	defer EraseByteBuffer(masterSeed)
+
+	var bip39Seed []byte
+
+	if w.bip39Seed != nil {
+		bip39Seed = w.decryptBip39Seed(key)
+		if bip39Seed == nil {
+			panic("decrypting bip39seed failed")
+		}
+		defer EraseByteBuffer(bip39Seed)
+	}
+
+	w.encryptMasterSeed(masterSeed, newKey)
+
+	if bip39Seed != nil {
+		w.encryptBip39Seed(bip39Seed, newKey)
+	}
+
+	for i, _ := range w.accounts {
+		a := &w.accounts[i]
+
+		if a.active && a.accountType == AccountTypeRandom {
+			pkey := decryptAccountSeed(a.privateKey, key)
+			if pkey == nil {
+				panic("decrypting account seed failed")
+			}
+			a.privateKey = encryptAccountSeed(pkey, newKey)
+			EraseByteBuffer(pkey)
+		}
+	}
+
+	return true
+}
+
+func (w *Wallet)newAccount() *Account {
+	for i, _ := range w.accounts {
+		a := &w.accounts[i]
+		if !a.active {
+			a.init(w)
+			return a
+		}
+	}
+
+	w.accounts = append(w.accounts, Account{})
+
+	a := &w.accounts[len(w.accounts)-1]
+
+	a.init(w)
+
+	return a
+}
+
+func (w *Wallet)GenerateSep0005Account(walletPassword *string) *Account {
+	if w.bip39Seed == nil {
+		return nil
+	}
+
+	wkey := w.checkPassword(walletPassword)
+
+	if wkey == nil {
+		return nil
+	}
+	defer EraseByteBuffer(wkey)
+	
+	seed := w.decryptBip39Seed(wkey)
+
+	if seed == nil {
+		return nil
+	}
+	defer EraseByteBuffer(seed)
+
+	a := w.newAccount()
+
+	p := fmt.Sprintf(derivation.StellarAccountPathFormat, w.sep0005AccountCount)
+
+	w.sep0005AccountCount++
+
+	var k, pk []byte
+	var err error
+	var key *derivation.Key
+
+	for i := 0; i < 5; i++ {
+		pk = k
+
+		key, err = derivation.DeriveForPath(p, seed)
+		if err != nil {
+			panic(err)
+		}
+
+		k = key.Key
+
+		if pk != nil && bytes.Compare(pk, k) != 0 {
+			panic("calculation error")
+		}
+	}
+
+	publicKey := derivePublicKey(key.Key)
+	if err != nil {
+		panic(err)
+	}
+
+	a.accountType = AccountTypeSEP0005
+	a.sep0005DerivationPath = p
+	a.publicKey, err = strkey.Encode(strkey.VersionByteAccountID, publicKey)
+
+	if err != nil {
+		panic(err)
+	}
+
+	a.active = true
+
+	return a
+}
+
+func encryptAccountSeed(seed, key []byte) []byte {
+	return encryptWithCheckSum(seed, key)
+}
+
+func decryptAccountSeed(encSeed, key []byte) []byte {
+	return decryptWithCheckSum(encSeed, accountSeedLen, key)
+}	
+
+func derivePublicKey( seed []byte ) []byte {
+	var prev, pub []byte
+	var err error
+
+	for i := 0; i < 5; i++ {
+		prev = pub
+
+		reader := bytes.NewReader(seed)
+		pub, _, err = ed25519.GenerateKey(reader)
+		if err != nil {
+			panic(err)
+		}
+
+		if prev != nil && bytes.Compare(prev, pub) != 0 {
+			panic("calculation error")
+		}
+	}
+	return pub
+}
+
+
+func (w *Wallet)AddRandomAccount(seed *string, walletPassword *string) *Account {
+	key := w.checkPassword(walletPassword)
+
+	if key == nil {
+		return nil
+	}
+	defer EraseByteBuffer(key)
+	
+	seedData, err := strkey.Decode(strkey.VersionByteSeed, *seed)
+	if err != nil {
+		return nil
+	}
+	defer EraseByteBuffer(seedData)
+
+	encSeed := encryptAccountSeed(seedData, key)
+
+	pubKey := derivePublicKey(seedData)
+
+	a := w.newAccount()
+
+	a.accountType = AccountTypeRandom
+	a.privateKey = encSeed
+	a.publicKey, err = strkey.Encode(strkey.VersionByteAccountID, pubKey)
+
+	if err != nil {
+		panic(err)
+	}
+
+	a.active = true
+
+	return a
+}
+
+func (w *Wallet)AddWatchingAccount(pubkey string) *Account {
+
+	_, err := strkey.Decode(strkey.VersionByteAccountID, pubkey)
+
+	if err != nil {
+		return nil
+	}
+
+	a := w.FindAccountByPublicKey(pubkey)
+
+	if a != nil {
+		return nil
+	}
+
+	a = w.newAccount()
+
+	a.accountType = AccountTypeWatching
+	a.publicKey = pubkey
+
+	a.active = true
+	
+	return a
+}
+
+func (w *Wallet)AddAddressBookAccount(pubkey string) *Account {
+
+	_, err := strkey.Decode(strkey.VersionByteAccountID, pubkey)
+
+	if err != nil {
+		return nil
+	}
+
+	a := w.FindAccountByPublicKey(pubkey)
+
+	if a != nil {
+		return nil
+	}
+
+	a = w.newAccount()
+
+	a.accountType = AccountTypeAddressBook
+	a.publicKey = pubkey
+
+	a.active = true
+	
+	return a
+}
+
+func (w *Wallet)DeleteAccount(acc *Account) bool {
+	if acc.wallet == w {
+		acc.active = false
+		return true
+	}
+
+	return false
+}
+
+func (w *Wallet)FindAccountByPublicKey(pubkey string) *Account {
+
+	for i, _ := range w.accounts {
+		if w.accounts[i].active &&  w.accounts[i].publicKey == pubkey {
+			return &w.accounts[i]
+		}
+	}
+
+	return nil
+}
+
+func (w *Wallet)FindAccountByDescription(desc string) *Account {
+	desc = strings.ToLower(desc)
+
+	for i, _ := range w.accounts {
+		if w.accounts[i].active {
+			if strings.Contains(strings.ToLower(w.accounts[i].desc), desc) {
+				return &w.accounts[i]
+			}
+		}
+	}
+
+	return nil
+}
+
+func (w *Wallet)GetAccounts() []*Account {
+	accounts := make([]*Account, 0, len(w.accounts))
+
+	for i, _ := range w.accounts {
+		if w.accounts[i].active && w.accounts[i].IsOwnAccount() {
+			accounts = append(accounts, &w.accounts[i])
+		}
+	}
+
+	return accounts
+}
+
+func (w *Wallet)GetAddressBook() []*Account {
+	accounts := make([]*Account, 0, len(w.accounts))
+
+	for i, _ := range w.accounts {
+		if w.accounts[i].active && w.accounts[i].IsAddressBookAccount() {
+			accounts = append(accounts, &w.accounts[i])
+		}
+	}
+
+	return accounts
+}
+
+func (a *Account)init(wallet *Wallet) {
+	a.wallet = wallet
+	a.accountType = AccountTypeUndefined
+	a.desc = ""
+	a.publicKey = ""
+	a.privateKey = nil
+	a.sep0005DerivationPath = ""
+}
+
+func (a *Account)Type() uint16 {
+	return a.accountType
+}
+
+func (a *Account)IsOwnAccount() bool {
+	switch a.accountType {
+	case AccountTypeSEP0005:
+		return true
+	case AccountTypeRandom:
+		return true
+	case AccountTypeWatching:
+		return true
+	}
+
+	return false
+}
+
+func (a *Account)IsAddressBookAccount() bool {
+	if a.accountType == AccountTypeAddressBook {
+		return true
+	}
+
+	return false
+}
+
+func (a *Account)HasPrivateKey() bool {
+	switch a.accountType {
+	case AccountTypeSEP0005:
+		return true
+	case AccountTypeRandom:
+		return true
+	}
+
+	return false
+}
+
+
+
+func (a *Account)GetDescription() string {
+	return a.desc
+}
+
+func (a *Account)SetDescription(desc string) {
+	a.desc = desc
+}
+
+
+
+func (a *Account)PublicKey() string {
+	if !a.active {
+		panic("account not active")
+	}
+	return a.publicKey
+}
+
+func (a *Account)PrivateKey(walletPassword *string) string {
+	wkey := a.wallet.checkPassword(walletPassword)
+
+	if wkey == nil {
+		return ""
+	}
+
+	defer EraseByteBuffer(wkey)
+
+	switch a.accountType {
+	case AccountTypeSEP0005:
+		seed := a.wallet.decryptBip39Seed(wkey)
+		if seed == nil {
+			return ""
+		}
+		defer EraseByteBuffer(seed)
+		key, err := derivation.DeriveForPath(a.sep0005DerivationPath, seed)
+		if err != nil {
+			return ""
+		}
+		s, err := strkey.Encode(strkey.VersionByteSeed, key.Key)
+		if err != nil {
+			return ""
+		}
+		return s
+
+	case AccountTypeRandom:
+		seed := decryptAccountSeed(a.privateKey, wkey)
+		if seed == nil {
+			panic("private key decryption failed")
+		}
+		defer EraseByteBuffer(seed)
+		s, err := strkey.Encode(strkey.VersionByteSeed, seed)
+		if err != nil {
+			panic(err)
+		}
+		return s
+	}
+
+	return ""
+}
+
